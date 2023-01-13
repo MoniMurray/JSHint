adfa7fd54adcbd7a1a80451c604e262ef52d7cec
Monica Murray <Monica.rickerby@gmail.com>
2023-01-13 17:16:26 +0000

    Insert API Key to script.js, include API URL

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// JSHint API - My personal key: CfZphzcUplq2fVcP9SqcGCTDqnk {"expiry":"13-01-2024","status_code":200}
+
+const API_KEY = "CfZphzcUplq2fVcP9SqcGCTDqnk";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
