456fdbcfc3454764eccfb9683031ebe8cb5d89fe
Monica Murray <Monica.rickerby@gmail.com>
2023-01-14 18:21:00 +0000

    Continue POST request code, include function to display Errors using dot notation to display the Error methods

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,26 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 //wire up the button with a standard event listener
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+                        method: "POST",
+                        headers: {
+                                    "Authorization": API_KEY,
+                                 },
+                        body: form
+                        });
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    };
+}
 
 //Asynchronous function - to handle a promise, wrap the promises in an async function and await the promise coming true.
 async function getStatus(e) {
@@ -28,8 +48,25 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data); 
         // or use dot notation to drill down for specific result like console.log(data.expiry) to get the API Key expiry date
+    } else {
+        throw new Error(data.error);
     }
 
+}
+
+function displayErrors(data) {
+    let heading = `JSHint Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class="no-errors">No Errors Reported!</div>`;
+    } else {
+        results =  `<div>Total Errors: <span class="error-count">${data.total_errors}</span></span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At Line <span class="line">${error.line}</span>,  `;
+            results += `column <span class="columns">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
 }
 
 function displayStatus(data) {
@@ -44,3 +81,5 @@ function displayStatus(data) {
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
 }
+
+
